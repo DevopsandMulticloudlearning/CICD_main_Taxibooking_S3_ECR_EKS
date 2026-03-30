72d443da20b4600b15a51213995c3d676f288bd9
Sai Ram Guthula <sairamguthula8@gmail.com>
2026-03-30 22:39:26 +0530

    Update taxi-infra.tf

--- terraform_files/taxi-infra.tf
+++ terraform_files/taxi-infra.tf
@@ -75,6 +75,14 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "App Port"
+    from_port   = 8001
+    to_port     = 8001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
